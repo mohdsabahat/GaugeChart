9f3fd7a8ac8ea9da50251e5f732df2a19805cfd5
mohdsabahat <mohdsabahat123@gmail.com>
2025-03-10 16:21:29 +0530

    chore: update project configuration files

--- release.config.js
+++ release.config.js
@@ -7,7 +7,7 @@ module.exports = {
         [
             "@semantic-release/git",
             {
-                "assets": ["package.json", "package-lock.json", "CHANGELOG.md"],
+                "assets": ["package.json", "package-lock.json", "CHANGELOG.md", "dist/**"],
                 "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
             }
         ],
